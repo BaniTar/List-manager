eb2ef7e16a11721197576e7394ab56f84c0e742d
Bani Tariveh <bani.tariveh@gmail.com>
2018-02-04 16:49:47 +0200

    Added functions for reading a list file from given path. Added a test run to main for them and a function that prints the list.

--- main.py
+++ main.py
@@ -11,12 +11,75 @@ def user_prompt(prompt_text):
     :return: read value
     """
     result = input(prompt_text)
-    # TODO: implement input validation here
+    # TODO: implement input validation here?
     return result
 
 
+def get_file_lines(path):
+    """
+    Reads the lines of a file into a list and returns it.
+    :param path: path to file
+    :return: either False or list of lines. False means error in reading.
+    """
+    # Checking for errors.
+    try:
+        file = open(path, "r")
+        lines = file.readlines()
+        file.close()
+
+    except OSError:
+        return False
+
+    # Clean lines.
+    for i in range(len(lines)):
+        lines[i] = lines[i].rstrip()
+
+    return lines
+
+
+def read_list_file(path):
+    """
+    Reads a file with list entries in it. Cleans and returns the lines.
+    :param path: filepath
+    :return: list of the file's lines
+    """
+    # Read contents.
+    lines = get_file_lines(path)
+
+    if lines is False:
+        print("Error when trying to access file.")
+        return []
+
+    elif "".join(lines) == "":
+        print("Error: file is empty")
+        return []
+
+    # Successful read.
+    else:
+        return lines
+
+
+def line_printer(lines):
+    """
+    Prints lines of a list line by line.
+    :param lines: list of strings
+    """
+    for line in lines:
+        print(line)
+
+
 def main():
-    list_path = user_prompt("Give path to list file.")
+    # Get file path from user.
+    list_path = user_prompt("Give path to list file:\n> ")
+    # Get list (of entries) from file.
+    list_entries = read_list_file(list_path)
+
+    # Test print the list.
+    if not list_entries:  # Faulty file.
+        return
+
+    else:
+        line_printer(list_entries)
 
 
 main()
